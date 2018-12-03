e3d1cebc934800b16b5e2269a4e5eec55d4b1e28
Anartz Mugika <mugan86@gmail.com>
2018-12-03 17:06:53 +0100

    Primer commit y proyecto 1.0.0

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+export function hola() {
+    console.log('Hola Mundo!!!');
+}
+
+export function holaPersonalizado(nombre: string) {
+    console.log(`Hola ${ nombre }`);
+}
+
+export function holaAlCursoNPM() {
+    console.log('Hola al curso NPM!!!');
+}
